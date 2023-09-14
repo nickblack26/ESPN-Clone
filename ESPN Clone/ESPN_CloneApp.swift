a0dce0e1a016df4ca302a76ebbd20d38dc2d4f35
Nick Black <49686365+nickblack26@users.noreply.github.com>
2023-09-14 09:57:19 -0500

    Initial Commit

---
+++ ESPN Clone/ESPN_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESPN_CloneApp.swift
+//  ESPN Clone
+//
+//  Created by Nick on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ESPN_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
